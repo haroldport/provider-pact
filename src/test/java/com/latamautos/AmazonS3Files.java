c00ff1b50bc15a6a5cca2cc7e9df83c4093a5ec9
Harold Portocarrero <hportocarrero@latamautos.com>
2016-12-01 14:02:01 -0500

    inclusion de amazon s3

---
+++ src/test/java/com/latamautos/AmazonS3Files.java
@@ -0,0 +1,70 @@
+package com.latamautos;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Harold on 1/12/16.
+ */
+public class AmazonS3Files {
+
+    public List<String> listfiles(String microserviceName){
+        AnonymousAWSCredentials awsCreds = new AnonymousAWSCredentials();
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+
+        try {
+            System.out.println("Listing objects");
+            List<String> urlList = new ArrayList<>();
+            String existingBucketName = "microservice-pacts";
+            final ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(existingBucketName).withPrefix(microserviceName + "/stage").withMaxKeys(2);
+            ListObjectsV2Result result;
+            do {
+                result = s3client.listObjectsV2(req);
+
+                for (S3ObjectSummary objectSummary :
+                        result.getObjectSummaries()) {
+                    System.out.println(" - " + objectSummary.getKey() + "  " +
+                            "(size = " + objectSummary.getSize() +
+                            ")");
+                    urlList.add("https://s3.amazonaws.com/microservice-pacts/" + objectSummary.getKey());
+                }
+                System.out.println("Next Continuation Token : " + result.getNextContinuationToken());
+                req.setContinuationToken(result.getNextContinuationToken());
+            } while(result.isTruncated());
+            return urlList;
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, " +
+                    "which means your request made it " +
+                    "to Amazon S3, but was rejected with an error response " +
+                    "for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to communicate" +
+                    " with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+        return new ArrayList<>();
+    }
+
+}
--- src/test/scala/com/latamautos/PactProviderTest.scala
+++ src/test/scala/com/latamautos/PactProviderTest.scala
@@ -4,12 +4,12 @@ package com.latamautos
   * Created by Harold on 25/11/16.
   */
 import java.io.File
+import java.net.URL
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.{Files, Paths}
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.Http.ServerBinding
 import com.atlassian.oai.validator.pact.PactProviderValidator
 import org.scalatest._
 import akka.stream.ActorMaterializer
@@ -17,8 +17,8 @@ import akka.util.Timeout
 import com.latamautos.resources.CorsSupport
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.collection.JavaConverters._
 
 class PactProviderTest extends FunSuiteLike with CorsSupport with RestInterface {
   val config = ConfigFactory.load()
@@ -38,22 +38,19 @@ class PactProviderTest extends FunSuiteLike with CorsSupport with RestInterface 
     println(s"REST interface could not bind to $host:$port", ex.getMessage)
   }
 
-  def stopWebServer(bindingFuture: Future[ServerBinding]): Unit = {
-    bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ => {
-        system.terminate
-      })
-  }
+  val amazonS3Files: AmazonS3Files = new AmazonS3Files
 
   val SWAGGER_URL = "http://localhost:5000/api-docs/swagger.json"
   val pactDir = "/Users/Harold/projects/provider-pact/src/main/resources/pacts"
 
   test("validateCmd WHEN messageId is empty SHOULD return (None, None)") {
-    val validator: PactProviderValidator = PactProviderValidator.createFor(SWAGGER_URL).withConsumer("ExampleConsumer", gelLastPactFile.get.getAbsolutePath).build
-    println("======================validator.validate() = " + validator.validate().hasErrors)
-    println("======================validator.validate() = " + validator.validate().getValidationFailureReport)
-    assert(!validator.validate().hasErrors)
+    amazonS3Files.listfiles("microservice-p1").asScala.foreach(url => {
+      println(s"url------>>>>>>>>>>>>>>>>>>> $url")
+      val validator: PactProviderValidator = PactProviderValidator.createFor(SWAGGER_URL).withConsumer("ExampleConsumer", new URL(url)).build
+      println("======================validator.validate() = " + validator.validate().hasErrors)
+      println("======================validator.validate() = " + validator.validate().getValidationFailureReport)
+      assert(!validator.validate().hasErrors)
+    })
   }
 
   def gelLastPactFile:Option[File] = {
